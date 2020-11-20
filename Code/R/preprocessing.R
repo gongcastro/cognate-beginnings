58fff9c92c87852f77dbb3ee19cff90dba795220
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-11-21 00:18:38 +0100

    Incorporate to MultiLex workflow

--- Code/R/preprocessing.R
+++ Code/R/preprocessing.R
@@ -4,15 +4,10 @@
 
 # load packages
 library(tidyverse)
-library(lubridate)
-library(readxl)
-library(janitor)
+library(multilex)
 library(data.table)
-library(mice)
+library(janitor)
 library(here)
-
-# load functions
-source(here("Code", "R", "functions.R"))
 
 # set params
 set.seed(888)
@@ -20,140 +15,57 @@ bins <- c("< 10", "10-12", "12-14", "14-16", "16-18", "18-20", "20-22", "22-24",
 bins_interest <- bins[-c(1, length(bins))]
 breaks <- c(0, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 100)
 
-#### participant data ######################
-
-participants <- fread(here("Data", "participants.csv"), na.strings = "") %>%
-    as_tibble() %>%
-    mutate_at(vars(date_sent, time_stamp), as_date) %>%
-    mutate_at(vars(id_db), factor) %>% 
-    mutate_at(vars(age_bin), factor, levels = bins, ordered = TRUE) %>%
-    mutate(dominance = case_when(id_db %in% "54469" ~ "Spanish",
-                                 id_db %in% "57157" ~ "Catalan",
-                                 TRUE ~ dominance),
-           doe_catalan = case_when(id_db %in% "54469" ~ 0,
-                                   id_db %in% "57157" ~ 80,
-                                   TRUE ~ as.numeric(doe_catalan))) %>% 
-    mutate(version = factor(version, levels = c("CBC", "DevLex", "BL-Short-A", "BL-Short-B", "BL-Short-C", "BL-Short-D", "BL-Long-1", "BL-Long-2"), ordered = TRUE),
-           doe1 = ifelse(dominance %in% "Spanish", doe_spanish, doe_catalan),
-           doe2 = ifelse(dominance %in% "Spanish", doe_catalan, doe_spanish),
-           lp = case_when(data.table::between(doe1, 40, 100, incbounds = TRUE) ~ lp,
-                          data.table::between(doe2, 0, 50, incbounds = TRUE) ~ lp,
-                          between(doe1 + doe2, 90, 100, incbounds = TRUE) ~  lp,
-                          TRUE ~ "Other")) %>% 
-    rowwise() %>% 
-    mutate(bilingualism = ifelse(dominance %in% "Spanish",
-                                 1-(abs((doe_spanish-doe_catalan)/(doe_spanish+doe_catalan))),
-                                 1-(abs((doe_catalan-doe_spanish)/(doe_spanish+doe_catalan))))) %>% 
-    drop_na(lp, bilingualism) %>% 
-    filter(completed, age_bin %in% bins_interest) 
-
-# export data
-fwrite(participants, here("Data", "participants.csv"), sep = ",", dec = ".", row.names = FALSE)
-
-
-#### items data ############################################################
-pool <- read_xlsx(here("Data", "pool.xlsx"), na = c("", "NA")) %>%
-    rename(te = meaning) %>% 
-    mutate(frequency = as.numeric(frequency),
-           te = as.character(te),
-           cognate_rater1 = ifelse(cognate_rater1==1, "Cognate", "Non-cognate")) %>%
-    drop_na(language, cognate_rater1)
-
+#### import data ######################
+participants_raw <- ml_participants("gonzalo.garciadecastro@upf.edu")
+responses_raw <- ml_responses(participants = participants_raw,
+                              formr_email = "gonzalo.garciadecastro@upf.edu")
+logs_raw <- ml_logs(responses = responses_raw, participants = participants_raw) %>% 
+    filter(age_bin %in% bins_interest,
+           lp %in% c("Monolingual", "Bilingual")) 
 
 #### process responses #####################################################
-responses <- fread(here("Data", "responses.csv"), header = TRUE, stringsAsFactors = FALSE, na.strings = "") %>%
-    as_tibble() %>%
-    mutate_at(vars(time_stamp), as_date) %>%
-    mutate_at(vars(id_db), as.character) %>%
-    mutate(dominance = case_when(id_db %in% "54469" ~ "Spanish",
-                                 id_db %in% "57157" ~ "Catalan",
-                                 TRUE ~ dominance),
-           doe_catalan = case_when(id_db %in% "54469" ~ 0,
-                                   id_db %in% "57157" ~ 80,
-                                   id_db %in% "57046" ~ 50,
-                                   TRUE ~ as.numeric(doe_catalan)),
-           doe_spanish = case_when(id_db %in% "57046" ~ 50,
-                                   TRUE ~ as.numeric(doe_spanish))) %>% 
-    drop_na(response) %>%
-    mutate(response = case_when(response == 1 ~ "no", response == 2 ~ "understands", response == 3 ~ "produces", TRUE ~ NA_character_),
+responses <- responses_raw %>% 
+    select(id, time, item, response, language) %>% 
+    left_join(logs_raw, by = c("id", "time")) %>% 
+    drop_na(response) %>% 
+    mutate(age_bin = factor(cut(age, breaks = breaks, labels = bins), levels = bins, ordered = TRUE),
+           item_dominance = ifelse(language==dominance, "L1", "L2"),
+           bilingualism = ifelse(dominance=="Catalan", doe_spanish, doe_catalan)) %>% 
+    select(id, age_bin, age, sex, dominance, bilingualism, time, item, language, item_dominance, response) %>% 
+    left_join(pool, by = c("item", "language")) %>% 
+    select(id, time, age_bin, age, sex, dominance, bilingualism, time, te, item, cognate, language, item_dominance, response) %>% 
+    mutate(cognate = case_when(cognate ~ "Cognate",
+                               !cognate ~ "Non-cognate",
+                               TRUE ~ NA_character_))
+
+fwrite(responses, here("Data", "responses.csv"), sep = ",", dec = ".", row.names = FALSE)
+
+#### vocabulary sizes ###########################################################################
+
+vocabulary <- responses %>% 
+    mutate(response = case_when(response == 1 ~ "no",
+                                response == 2 ~ "understands",
+                                response == 3 ~ "produces",
+                                TRUE ~ NA_character_),
            understands = response %in% c("understands", "produces"),
-           produces = response %in% "produces",
-           doe1 = ifelse(dominance %in% "Spanish", doe_spanish, doe_catalan),
-           doe2 = ifelse(dominance %in% "Spanish", doe_catalan, doe_spanish),
-           age_bin = factor(cut(age, breaks = breaks, labels = bins), levels = bins, ordered = TRUE)) %>%
-    rowwise() %>% 
-    mutate(doe_total = doe1 + doe2) %>% 
-    ungroup() %>% 
-    mutate(lp = case_when(!data.table::between(doe1, 40, 100, incbounds = TRUE) ~ "Other",
-                          !data.table::between(doe2, 0, 50, incbounds = TRUE) ~ "Other",
-                          !between(doe1 + doe2, 90, 100, incbounds = TRUE) ~  "Other",
-                          TRUE ~ lp)) %>% 
+           produces = response %in% "produces") %>% 
     filter(lp %in% c("Monolingual", "Bilingual"),
            age_bin %in% bins_interest) %>%
     select(-response) %>% 
     pivot_longer(c(understands, produces), names_to = "type", values_to = "response") %>%
     mutate(type = ifelse(type=="understands", "Comprehensive", "Productive")) %>%
-    arrange(item, lp, item_dominance, age_bin)
-
-fwrite(responses, here("Data", "raw.csv"), sep = ",", dec = ".", row.names = FALSE)
-
-
-#### vocabulary sizes ###########################################################################
-studies <- fread(here("Data", "studies.csv"), na.strings = c("", "NA")) %>%
-    distinct(q_version, language, q_items)
-
-vocabulary <- responses %>% 
-    as_tibble() %>%
-    mutate_at(vars(id_db), as.character) %>%
-    mutate(version = factor(version, levels = c("CBC", paste0("BL-Short-", c("A", "B", "C", "D")),
-                                                paste0("BL-Long-", 1:2),
-                                                "DevLex",
-                                                paste0("BL-Lockdown-", c("A", "B", "C", "D")),
-                                                ordered = TRUE))) %>% 
-    left_join(studies, by = c("version" = "q_version", "language")) %>% 
-    select(id_db, study, version, age, age_bin, sex, language, item_dominance, dominance, doe1, doe2, lp, type, response, q_items) %>% 
-    rename(dominance_doe = dominance) %>%
-    arrange(id_db, type, language) %>% 
+    select(id, time, age, age_bin, sex, language, item_dominance, dominance, lp, type, response) %>% 
+    filter(sex %in% c("Female", "Male")) %>% 
     drop_na(response) %>% 
-    group_by(id_db, age, sex, type, study, version, language, lp, dominance_doe, item_dominance, doe1, doe2, q_items) %>%
-    summarise(n = n(),
-              sum = sum(response, na.rm = TRUE),
-              vocab_size = prod(sum, 1/n, na.rm = TRUE),
-              .groups = "drop") %>%  
-    ungroup() %>%
-    clean_names() %>% 
+    group_by(id, time, age, sex, type, language, lp, item_dominance) %>%
+    summarise(yes = sum(response, na.rm = TRUE),
+              n = sum(!is.na(response)),
+              .groups = "drop") %>% 
     rowwise() %>% 
-    ungroup() %>% 
-    select(-c(q_items))
+    mutate(vocab = yes/n) %>% 
+    ungroup()
 
 fwrite(vocabulary, here("Data", "vocabulary.csv"), sep = ",", dec = ".", row.names = FALSE)
-
-
-#### aggregate data #############################################################################
- aggregated <- responses %>% 
-    group_by(item, version, sex, lp, age_bin, item_dominance, type) %>%
-    summarise(n = sum(!is.na(response)),
-              successes = sum(response, na.rm = TRUE),
-              proportion = mean(response, na.rm = TRUE),
-              .groups = "drop") %>%
-     ungroup() %>%
-     arrange(age_bin, lp, item_dominance) %>%
-     left_join(select(pool, -version), by = "item") %>%
-     rename(cognate = cognate_rater1) %>%
-     mutate_at(vars(include), ~as.logical(as.numeric(.))) %>%
-     filter(include) %>%
-     drop_na(cognate, age_bin) %>%
-     select(-c(survey, ipa, source, label, A, B, C, D, include, cognate_expert, cognate_rater2, agreement, comments))
-
-#### prepare data ##############################################################
-prepared <- aggregated %>%
-    filter(class %in% c("nouns"),
-           !(type %in% "Productive" & version %in% "DevLex")) %>%
-    select(item, version, type, te, age_bin, category, item_dominance, cognate, proportion, successes, n) %>%
-    arrange(type, item, te, age_bin)
-
-fwrite(prepared, here("Data", "preprocessed.csv"), sep = ",", dec = ".", row.names = FALSE)
-
 
 
 
