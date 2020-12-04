6f5b0a8b893bb32dbfef97f1abd5a6dca94a00ba
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-12-04 18:47:48 +0100

    Update analysis

---
+++ R/analyses.R
@@ -0,0 +1,210 @@
+#### set up --------------------------------------------------------------------
+
+# load packages
+library(tidyverse)
+library(multilex)
+library(data.table)
+library(brms)
+library(tidybayes)
+library(here)
+
+#### import data ---------------------------------------------------------------
+participants <- ml_participants("gonzalo.garciadecastro@upf.edu")
+responses <- ml_responses(participants, "gonzalo.garciadecastro@upf.edu")  
+logs <- ml_logs(participants, responses)
+vocabulary <- ml_vocabulary(participants, responses)
+
+#### vocabulary sizes ----------------------------------------------------------
+dat_vocab <- logs %>% 
+    filter(
+        completed,
+        !(version %in% c("DevLex", "CBC")),
+        lp %in% c("Monolingual", "Bilingual"),
+        between(age, 10, 40),
+    ) %>% 
+    select(id, time, age, doe_catalan, doe_spanish) %>% 
+    left_join(
+        vocabulary, by = c("id", "time")
+    ) %>% 
+    group_by(id) %>% 
+    filter(time==min(.$time)) %>% 
+    ungroup() %>% 
+    mutate(
+        bilingualism = case_when(
+            doe_catalan > doe_spanish ~ doe_spanish/100,
+            doe_spanish > doe_catalan ~ doe_catalan/100,
+            TRUE ~ 0.5
+        ),
+        vocab_type = ifelse(vocab_type=="understands", 0, 1),
+        id = as.numeric(as.factor(id))
+    ) %>% 
+    mutate_at(vars(vocab_type), as.factor) 
+
+
+#### process responses ---------------------------------------------------------
+dat_responses <- responses %>% 
+    select(id, time, item, response, language) %>% 
+    left_join(logs, by = c("id", "time")) %>% 
+    mutate(
+        item_dominance = ifelse(language==dominance, "L1", "L2"),
+        bilingualism = ifelse(dominance=="Catalan", doe_spanish, doe_catalan)/100
+    ) %>% 
+    select(id, version, age, sex, dominance, lp, bilingualism, time, item, language, item_dominance, response, completed) %>% 
+    left_join(select(pool, te, language, item, category, class, frequency, cognate, include),
+              by = c("item", "language")) %>% 
+    mutate(cognate = case_when(
+        cognate ~ "Cognate",
+        !cognate ~ "Non-cognate",
+        TRUE ~ NA_character_)
+    ) %>% 
+    filter(
+        completed,
+        !(version %in% c("DevLex", "CBC")),
+        lp %in% c("Monolingual", "Bilingual"),
+        between(age, 10, 40),
+        include,
+        class %in% c("Verb", "Noun", "Adjective")
+    ) %>% 
+    group_by(id) %>% 
+    filter(time==min(.$time)) %>% 
+    ungroup() %>% 
+    drop_na(response, te, cognate) %>% 
+    select(id, age, sex, dominance, lp, te, item, response, language, item_dominance, cognate, frequency, bilingualism) %>% 
+    mutate(age_centred = scale(age, center = TRUE, scale = FALSE)[,1]) %>%
+    mutate_at(vars(cognate, item_dominance), as.factor) %>%
+    mutate(response = factor(response, ordered = TRUE, labels = c("No", "Understands", "Understands & Says"))) %>% 
+    arrange(item, te) %>% 
+    select(id, age, bilingualism, item, te, item_dominance, cognate, response) 
+
+contrasts(dat_responses$response) <- c(0, 1, 2)
+contrasts(dat_responses$cognate) <- c(1, 0)
+contrasts(dat_responses$item_dominance) <- c(0, 1)
+
+#### model fitting -------------------------------------------------------------
+priors <- c(
+    prior(normal(0.5, 0.5), class = Intercept),
+    prior(normal(0.1, 0.15), class = b, coef = "age_centred"),
+    prior(normal(0, 0.5), class = b, coef = "item_dominance1"),
+    prior(normal(0, 0.5), class = b, coef = "age_centred:item_dominance1"),
+    prior(normal(0, 0.5), class = b, coef = "cognateness1"),
+    prior(normal(0, 0.5), class = b, coef = "age_centred:cognateness1"),
+    prior(normal(0, 0.5), class = b, coef = "item_dominance1:cognateness1"),
+    prior(normal(0, 0.5), class = b, coef = "age_centred:item_dominance1:cognateness1")
+)
+
+fit0 <- brm(
+    response ~ cs(age),
+    data = dat_responses,
+    family = acat(link = "logit"),
+    prior = priors[1:2,],
+    save_all_pars = TRUE,
+    sample_prior = "yes",
+    save_model = here("Stan", "responses0.stan"),
+    file = here("Results", "responses0.rds"),
+    seed = 888, iter = 4000, chains = 4, init = 0, cores = 4
+)
+
+fit1 <- update(
+    fit0, . ~ . -cs(age) + cs(age*item_dominance),
+    newdata = dat_responses,
+    file = here("Results", "responses1.rds"),
+    save_model = here("Stan", "responses1.stan")
+)
+
+fit2 <- update(
+    fit1, . ~ . + cs(age*item_dominance*cognate),
+    newdata = dat_responses,
+    file = here("Results", "responses2.rds"),
+    save_model = here("Stan", "responses2.stan")
+)
+
+#### model comparison ----------------------------------------------------------
+map2(list(fit0, fit1, fit2, fit3),
+     list(here("Results", "responses0.rds"),
+          here("Results", "responses1.rds"),
+          here("Results", "responses2.rds")),
+     ~add_criterion(
+         .x,
+         criterion = c("loo", "waic", "bayes_R2", "loo_R2", "marglik"),
+         file = .y,
+         overwrite = TRUE
+     ))
+loos <- loo(fit0, fit1, fit2)
+waic <- WAIC(fit0, fit1, fit2)
+
+#### posterior -----------------------------------------------------------------
+post <- gather_draws(fit2, `b.*`, regex = TRUE) %>% 
+    mutate(.chain = as.factor(.chain)) %>% 
+    mutate(category = case_when(
+        str_detect(.variable, "\\[1\\]") ~ "No-Understands",
+        str_detect(.variable, "\\[2\\]") ~ "Understands-Produces"
+    ),
+    .variable = str_remove_all(.variable, "\\[.*?\\]")
+    )
+
+ggplot(post, aes(x = .iteration, y = .value, colour = .chain)) +
+    facet_wrap(category~.variable, scales = "free_y") +
+    geom_line() +
+    scale_colour_manual(values = c("#543005", "#BF812D", "#80CDC1", "#01665E")) +
+    theme_minimal() +
+    labs(x = "Iteration", y = "Value", colour = "Chain") +
+    theme(axis.title = element_text(face = "bold"),
+          strip.text = element_text(face = "bold"),
+          legend.title = element_text(face = "bold"),
+          legend.position = "top")
+
+ggplot(post, aes(.value, fill = category)) +
+    facet_wrap(~.variable, scales = "free") +
+    stat_halfeye() +
+    labs(x = "Value", y = "Probability density",
+         fill = "CrI") +
+    #scale_fill_brewer(palette = "Oranges", direction = -1,) +
+    theme_minimal() +
+    theme(axis.title = element_text(face = "bold"),
+          strip.text = element_text(face = "bold"),
+          strip.background = element_rect(fill = "grey", colour = NA),
+          legend.title = element_text(face = "bold"),
+          legend.position = "top") +
+    ggsave(here("Figures", "responses-coefs.png"))
+
+#### posterior predictions -----------------------------------------------------
+post_preds <- expand_grid(
+    age = seq(min(dat$age, na.rm = TRUE), max(dat$age, na.rm = TRUE), by = 0.5),
+    item_dominance = c("L1", "L2"),
+    cognate = c("Cognate", "Non-cognate")
+) %>% 
+    add_fitted_draws(., fit2, n = 50) %>% 
+    mutate(
+        #bilingualism = ifelse(bilingualism==0, "Monolingual", "Bilingual"),
+        .category = case_when(
+            .category==1 ~ "No",
+            .category==2 ~ "Understands",
+            .category==3 ~ "Understands & Produces"
+        )
+    )
+
+ggplot(post_preds,
+       aes(age, .value,
+           colour = interaction(item_dominance, cognate, lex.order = TRUE, sep = " - "),
+           fill = interaction(item_dominance, cognate, lex.order = TRUE, sep = " - ")
+       )) +
+    facet_wrap(~.category) +
+    stat_lineribbon(.width = 0.95, alpha = 0.5) +
+    stat_summary(fun = mean, geom = "line", size = 1) +
+    scale_fill_brewer(palette = "Dark2") +
+    scale_colour_brewer(palette = "Dark2") +
+    scale_y_continuous(limits = c(0, 1)) +
+    scale_x_continuous(breaks = seq(-14, 17, by = 5), labels = seq(10, 40, by = 5)) +
+    labs(x = "Age (months)", y = "P(Y|X)") +
+    theme_minimal() +
+    theme(axis.title = element_text(face = "bold"),
+          strip.text = element_text(face = "bold"),
+          strip.background = element_rect(fill = "grey", colour = NA),
+          legend.title =element_blank(),
+          legend.position = "top",
+          text = element_text(size = 15)) +
+    ggsave(here("Figures", "responses-multinomial.png"), height = 4)
+
+
+
+
--- Stan/responsesp.stan
+++ Stan/responsesp.stan
@@ -1,6 +1,6 @@
 // generated with brms 2.13.5
 functions {
-  /* sratio-logit log-PDF for a single response
+  /* acat-logit log-PDF for a single response
    * Args:
    *   y: response category
    *   mu: latent mean parameter
@@ -9,23 +9,17 @@ functions {
    * Returns:
    *   a scalar to be added to the log posterior
    */
-   real sratio_logit_lpmf(int y, real mu, real disc, vector thres) {
+   real acat_logit_lpmf(int y, real mu, real disc, vector thres) {
      int nthres = num_elements(thres);
      vector[nthres + 1] p;
-     vector[nthres] q;
-     int k = 1;
-     while (k <= min(y, nthres)) {
-       q[k] = 1 - inv_logit(disc * (thres[k] - mu));
-       p[k] = 1 - q[k];
-       for (kk in 1:(k - 1)) p[k] = p[k] * q[kk];
-       k += 1;
+     p[1] = 0.0;
+     for (k in 1:(nthres)) {
+       p[k + 1] = p[k] + disc * (mu - thres[k]);
      }
-     if (y == nthres + 1) {
-       p[nthres + 1] = prod(q);
-     }
-     return log(p[y]);
+     p = exp(p);
+     return log(p[y] / sum(p));
    }
-  /* sratio-logit log-PDF for a single response and merged thresholds
+  /* acat-logit log-PDF for a single response and merged thresholds
    * Args:
    *   y: response category
    *   mu: latent mean parameter
@@ -35,48 +29,43 @@ functions {
    * Returns:
    *   a scalar to be added to the log posterior
    */
-   real sratio_logit_merged_lpmf(int y, real mu, real disc, vector thres, int[] j) {
-     return sratio_logit_lpmf(y | mu, disc, thres[j[1]:j[2]]);
+   real acat_logit_merged_lpmf(int y, real mu, real disc, vector thres, int[] j) {
+     return acat_logit_lpmf(y | mu, disc, thres[j[1]:j[2]]);
    }
 }
 data {
   int<lower=1> N;  // number of observations
   int Y[N];  // response variable
   int<lower=2> nthres;  // number of thresholds
-  int<lower=1> K;  // number of population-level effects
-  matrix[N, K] X;  // population-level design matrix
+  int<lower=1> Kcs;  // number of category specific effects
+  matrix[N, Kcs] Xcs;  // category specific design matrix
   int prior_only;  // should the likelihood be ignored?
 }
 transformed data {
-  int Kc = K;
-  matrix[N, Kc] Xc;  // centered version of X
-  vector[Kc] means_X;  // column means of X before centering
-  for (i in 1:K) {
-    means_X[i] = mean(X[, i]);
-    Xc[, i] = X[, i] - means_X[i];
-  }
 }
 parameters {
-  vector[Kc] b;  // population-level effects
   vector[nthres] Intercept;  // temporary thresholds for centered predictors
+  matrix[Kcs, nthres] bcs;  // category specific effects
 }
 transformed parameters {
   real<lower=0> disc = 1;  // discrimination parameters
 }
 model {
+  // linear predictor for category specific effects
+  matrix[N, nthres] mucs = Xcs * bcs;
   // initialize linear predictor term
-  vector[N] mu = Xc * b;
+  vector[N] mu = rep_vector(0, N);
   // priors including all constants
-  target += normal_lpdf(b | 0.5, 0.1);
   target += normal_lpdf(Intercept | 0.5, 0.5);
+  target += normal_lpdf(to_vector(bcs) | 0, 0.1);
   // likelihood including all constants
   if (!prior_only) {
     for (n in 1:N) {
-      target += sratio_logit_lpmf(Y[n] | mu[n], disc, Intercept);
+      target += acat_logit_lpmf(Y[n] | mu[n], disc, Intercept - mucs[n]');
     }
   }
 }
 generated quantities {
   // compute actual thresholds
-  vector[nthres] b_Intercept = Intercept + dot_product(means_X, b);
+  vector[nthres] b_Intercept = Intercept;
 }
