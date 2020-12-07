759958b79b5a26b740f7243aec91761451894e49
gongcastro <gonzalo.garciadecastro@upf.edu>
2020-12-07 17:38:55 +0100

    Prepare introduction

--- R/analyses.R
+++ R/analyses.R
@@ -40,7 +40,6 @@ dat_vocab <- logs %>%
     ) %>% 
     mutate_at(vars(vocab_type), as.factor) 
 
-
 #### process responses ---------------------------------------------------------
 dat_responses <- responses %>% 
     select(id, time, item, response, language) %>% 
@@ -70,9 +69,12 @@ dat_responses <- responses %>%
     ungroup() %>% 
     drop_na(response, te, cognate) %>% 
     select(id, age, sex, dominance, lp, te, item, response, language, item_dominance, cognate, frequency, bilingualism) %>% 
-    mutate(age_centred = scale(age, center = TRUE, scale = FALSE)[,1]) %>%
+    #mutate(age_centred = scale(age, center = TRUE, scale = FALSE)[,1]) %>%
     mutate_at(vars(cognate, item_dominance), as.factor) %>%
-    mutate(response = factor(response, ordered = TRUE, labels = c("No", "Understands", "Understands & Says"))) %>% 
+    mutate(response = factor(
+        response, ordered = TRUE,
+        labels = c("No", "Understands", "Understands & Says"))
+    ) %>% 
     arrange(item, te) %>% 
     select(id, age, bilingualism, item, te, item_dominance, cognate, response) 
 
@@ -83,24 +85,25 @@ contrasts(dat_responses$item_dominance) <- c(0, 1)
 #### model fitting -------------------------------------------------------------
 priors <- c(
     prior(normal(0.5, 0.5), class = Intercept),
-    prior(normal(0.1, 0.15), class = b, coef = "age_centred"),
+    prior(normal(0.1, 0.15), class = b, coef = "age"),
     prior(normal(0, 0.5), class = b, coef = "item_dominance1"),
-    prior(normal(0, 0.5), class = b, coef = "age_centred:item_dominance1"),
+    prior(normal(0, 0.5), class = b, coef = "age:item_dominance1"),
     prior(normal(0, 0.5), class = b, coef = "cognateness1"),
-    prior(normal(0, 0.5), class = b, coef = "age_centred:cognateness1"),
+    prior(normal(0, 0.5), class = b, coef = "age:cognateness1"),
     prior(normal(0, 0.5), class = b, coef = "item_dominance1:cognateness1"),
-    prior(normal(0, 0.5), class = b, coef = "age_centred:item_dominance1:cognateness1")
+    prior(normal(0, 0.5), class = b, coef = "age:item_dominance1:cognateness1")
 )
 
 fit0 <- brm(
     response ~ cs(age),
     data = dat_responses,
-    family = acat(link = "logit"),
+    family = acat(link = "logit", threshold = "flexible"),
     prior = priors[1:2,],
     save_all_pars = TRUE,
     sample_prior = "yes",
     save_model = here("Stan", "responses0.stan"),
     file = here("Results", "responses0.rds"),
+    control = list(adapt_delta = 0.8, max_treedepth = 10),
     seed = 888, iter = 4000, chains = 4, init = 0, cores = 4
 )
 
@@ -119,7 +122,7 @@ fit2 <- update(
 )
 
 #### model comparison ----------------------------------------------------------
-map2(list(fit0, fit1, fit2, fit3),
+map2(list(fit0, fit1, fit2, fit2),
      list(here("Results", "responses0.rds"),
           here("Results", "responses1.rds"),
           here("Results", "responses2.rds")),
@@ -129,20 +132,48 @@ map2(list(fit0, fit1, fit2, fit3),
          file = .y,
          overwrite = TRUE
      ))
+
 loos <- loo(fit0, fit1, fit2)
 waic <- WAIC(fit0, fit1, fit2)
+bf <-
 
+#### test interactions ---------------------------------------------------------
+
+h <- hypothesis(
+    fit2,
+    class = "bcs", seed = 888,
+    c("cognate1[1] + item_dominance1[1] = 0",
+      "cognate1[1] + age:item_dominance1[1] = 0",
+      "item_dominance1[1] + cognate1[1] = 0",
+      "item_dominance1[1] + age:cognate1[1] = 0",
+      "cognate1[2] + item_dominance1[2] = 0",
+      "cognate1[2] + age:item_dominance1[2] = 0",
+      "item_dominance1[2] + cognate1[2] = 0",
+      "item_dominance1[2] + age:cognate1[2] = 0",
+      "age[1] = age[2]",
+      "item_dominance1[1] = item_dominance1[2]",
+      "cognate1[1] = cognate1[2]",
+      "age:item_dominance1[1] = age:item_dominance1[2]",
+      "age:cognate1[1] = age:cognate1[2]",
+      "age:item_dominance1:cognate1[1] = age:item_dominance1:cognate1[2]"
+    )
+)
 #### posterior -----------------------------------------------------------------
-post <- gather_draws(fit2, `b.*`, regex = TRUE) %>% 
-    mutate(.chain = as.factor(.chain)) %>% 
-    mutate(category = case_when(
-        str_detect(.variable, "\\[1\\]") ~ "No-Understands",
-        str_detect(.variable, "\\[2\\]") ~ "Understands-Produces"
-    ),
-    .variable = str_remove_all(.variable, "\\[.*?\\]")
+post <- list(posterior = gather_draws(fit0, `b.*`, regex = TRUE),
+             prior = gather_draws(fit0, `prior.*`, regex = TRUE)) %>% 
+    bind_rows(.id = "dist") %>% 
+    mutate(.chain = as.factor(.chain),
+           category = case_when(
+               str_detect(.variable, "\\[1\\]") ~ "No-Understands",
+               str_detect(.variable, "\\[2\\]") ~ "Understands-Produces",
+               str_detect(.variable, "prior") ~ "Prior"
+           ),
+           .variable = str_remove(.variable, "prior_"),
+           .variable = str_remove_all(.variable, "\\[.*?\\]")
     )
 
-ggplot(post, aes(x = .iteration, y = .value, colour = .chain)) +
+post %>% 
+    ggplot(aes(x = .iteration, y = .value, colour = .chain)) +
     facet_wrap(category~.variable, scales = "free_y") +
     geom_line() +
     scale_colour_manual(values = c("#543005", "#BF812D", "#80CDC1", "#01665E")) +
@@ -153,7 +184,9 @@ ggplot(post, aes(x = .iteration, y = .value, colour = .chain)) +
           legend.title = element_text(face = "bold"),
           legend.position = "top")
 
-ggplot(post, aes(.value, fill = category)) +
+post %>% 
+    filter(category != "Prior") %>% 
+    ggplot(aes(.value, fill = category)) +
     facet_wrap(~.variable, scales = "free") +
     stat_halfeye() +
     labs(x = "Value", y = "Probability density",
@@ -169,11 +202,11 @@ ggplot(post, aes(.value, fill = category)) +
 
 #### posterior predictions -----------------------------------------------------
 post_preds <- expand_grid(
-    age = seq(min(dat$age, na.rm = TRUE), max(dat$age, na.rm = TRUE), by = 0.5),
-    item_dominance = c("L1", "L2"),
-    cognate = c("Cognate", "Non-cognate")
+    age = seq(min(dat_responses$age, na.rm = TRUE), max(dat_responses$age, na.rm = TRUE), by = 0.5),
+    #item_dominance = c("L1", "L2"),
+    #cognate = c("Cognate", "Non-cognate")
 ) %>% 
-    add_fitted_draws(., fit2, n = 50) %>% 
+    add_fitted_draws(., fit0, n = 50) %>% 
     mutate(
         #bilingualism = ifelse(bilingualism==0, "Monolingual", "Bilingual"),
         .category = case_when(
@@ -185,8 +218,8 @@ post_preds <- expand_grid(
 
 ggplot(post_preds,
        aes(age, .value,
-           colour = interaction(item_dominance, cognate, lex.order = TRUE, sep = " - "),
-           fill = interaction(item_dominance, cognate, lex.order = TRUE, sep = " - ")
+           #colour = interaction(item_dominance, cognate, lex.order = TRUE, sep = " - "),
+           #fill = interaction(item_dominance, cognate, lex.order = TRUE, sep = " - ")
        )) +
     facet_wrap(~.category) +
     stat_lineribbon(.width = 0.95, alpha = 0.5) +
@@ -204,7 +237,5 @@ ggplot(post_preds,
           legend.position = "top",
           text = element_text(size = 15)) +
     ggsave(here("Figures", "responses-multinomial.png"), height = 4)
-
-
 
 
