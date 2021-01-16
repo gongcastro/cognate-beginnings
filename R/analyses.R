9041e9cd24edfffc4ead691dd5d8ac91dbfed2c2
gongcastro <gonzalo.garciadecastro@upf.edu>
2021-01-16 09:55:43 +0100

    Extract posterior d raws

--- R/analyses.R
+++ R/analyses.R
@@ -30,14 +30,13 @@ proportion <- responses %>%
   mutate(type = str_to_sentence(type)) %>% 
   group_by(age, frequency, bilingualism, cognate, dominance, type) %>%
   summarise(
-    .value = log((.value+0.5)/(n()-.value+0.5)),
+    .value = (.value+0.5)/(n()-.value+0.5),
     n = n(),
     .groups = "drop"
   ) %>% 
   rowwise() %>% 
   mutate(.value = prop_adj(.value, n)) %>% 
   ungroup() 
-
 
 # set sum contrasts (Schad et al., 2018, https://arxiv.org/abs/1807.10451)
 contrasts(responses$dominance) <- contr.sum(2)/2
@@ -174,7 +173,6 @@ prod_7 <- update(
 #### model comparison ----------------------------------------------------------
 loos_comp <- loo_subsample(comp_0, comp_1, comp_2, comp_3, comp_4, comp_5, comp_6, comp_7, nsamples = 500)
 loos_prod <- loo_subsample(prod_0, prod_1, prod_2, prod_3, prod_4, prod_5, prod_6, prod_7, nsamples = 500)
-
 saveRDS(list(loos_comp, loos_prod), here("Results", "loos.rds"))
 
 #### test interactions ---------------------------------------------------------
@@ -195,12 +193,18 @@ hypotheses <- hypothesis(
 #### examine posterior ---------------------------------------------------------
 
 # extract posterior draws for estimated parameters
-post <- gather_draws(comp_7, `b.*`, regex = TRUE) %>% 
+post <- bind_rows(
+  list(
+    Understands = gather_draws(comp_7, `b.*`, regex = TRUE),
+    Produces = gather_draws(prod_7, `b.*`, regex = TRUE)
+  ),
+  .id = "type"
+) %>% 
   mutate(.chain = as.factor(.chain)) 
 
 # check convergence of MCMC chains
 ggplot(post, aes(x = .iteration, y = .value, colour = .chain)) +
-  facet_wrap(~.variable, scales = "free_y", nrow = 4) +
+  facet_wrap(type~.variable, scales = "free_y", nrow = 4) +
   geom_line() +
   labs(x = "Iteration", y = "Value", colour = "Chain") +
   scale_colour_brewer(palette = "Dark2", direction = -1) +
@@ -210,6 +214,7 @@ ggplot(post, aes(x = .iteration, y = .value, colour = .chain)) +
 
 # posterior density by parameter
 ggplot(post, aes(exp(.value), .variable)) +
+  facet_wrap(~type, ncol = 1) +
   stat_slab(aes(fill = stat(cut_cdf_qi(cdf, .width = c(.5, .8, .95, 0.99), # quantiles
                                        labels = percent_format(accuracy = 1))))) +
   geom_vline(xintercept = 1, linetype = "dashed") +
@@ -218,14 +223,14 @@ ggplot(post, aes(exp(.value), .variable)) +
   scale_fill_brewer(palette = "Oranges", direction = -1, na.translate = FALSE) +
   theme_custom() +
   theme(legend.position = "top") +
-  ggsave(here("Figures", "coefs.png"))
+  ggsave(here("Figures", "coefs.png"), height = 5)
 
 #### posterior predictions -----------------------------------------------------
 
 # values to get posterior draws for (set frequency at mean = 0)
 nd <- expand_grid(
   n = 1,
-  age = seq(min(responses$age), max(responses$age), 1),
+  age = seq(min(responses$age), max(responses$age), 0.1),
   dominance = c("L1", "L2"),
   bilingualism = c(min(responses$bilingualism), 0, max(responses$bilingualism)),
   frequency = 0,
@@ -233,7 +238,13 @@ nd <- expand_grid(
 )
 
 # get 20 posterior draws
-post_preds <- add_fitted_draws(nd, comp_7, n = 50) %>% 
+post_preds <- bind_rows(
+  list(
+    Understands = add_fitted_draws(nd, comp_7, n = 20),
+    Produces = add_fitted_draws(nd, prod_7, n = 20)
+  ),
+  .id = "type"
+) %>% 
   mutate(bilingualism = factor(
     bilingualism,
     levels = unique(.$bilingualism),
@@ -246,11 +257,11 @@ post_preds %>%
     colour = interaction(dominance, cognate, sep = " - "),
     fill = interaction(dominance, cognate, sep = " - ")
   )) +
-  facet_wrap(~bilingualism) +
+  facet_grid(type~bilingualism) +
   #geom_line(aes(group = interaction(item_dominance, cognate, doe, .draw)), size = 0.4) +
   stat_lineribbon(.width = 0.95, colour = NA, alpha = 0.5) +
   stat_summary(fun = "mean", geom = "line", size = 0.75) +
-  #geom_point(data = filter(proportion, type=="Understands"), alpha = 0.5) +
+  #geom_point(data = proportion, alpha = 0.5) +
   labs(x = "Age (months)", y = "P(Y|X)") +
   guides(colour = guide_legend(ncol = 2)) +
   scale_color_brewer(palette = "Dark2") +
@@ -262,4 +273,4 @@ post_preds %>%
     legend.title = element_blank(),
     legend.direction = "horizontal"
   ) +
-  ggsave(here("Figures", "post-preds.png"), width = 7)
+  ggsave(here("Figures", "post-preds.png"), width = 8, height = 5)
