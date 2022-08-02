0cf677ca28a58bb10008baaf3add035474128c4c
Gonzalo García-Castro <31894043+gongcastro@users.noreply.github.com>
2022-08-02 11:29:04 +0200

    Fix dependencies

--- _targets.R
+++ _targets.R
@@ -2,60 +2,101 @@ library(targets)
 library(tarchetypes)
 
 source("R/utils.R")
-source("R/00_items.R")
-source("R/01_participants.R")
-source("R/02_vocabulary.R")
-source("R/03_responses.R")
-source("R/04_models.R")
+source("R/00_importing.R")
+source("R/01_preprocessing.R")
+source("R/02_models.R")
 
 # set parameters ----
+
 tar_option_set(
     packages = c(
-        "dplyr", "tidyr", "stringr", "ggplot2", "tibble", "forcats", "multilex", "keyring",
-        "readxl", "janitor", "mice", "here", "lubridate", "purrr", "scales",
-        "brms", "tidybayes", "gt", "patchwork", "wesanderson", "papaja", "knitr"
+        "arrow",
+        "brms", 
+        "conflicted",
+        "dplyr", 
+        "forcats",
+        "ggplot2", 
+        "ggsci",
+        "gt", 
+        "here", 
+        "janitor",
+        "keyring",
+        "knitr",
+        "lubridate",
+        "mice", 
+        "multilex",
+        "patchwork", 
+        "papaja", 
+        "purrr", 
+        "readxl", 
+        "rlang",
+        "scales", 
+        "stringdist",
+        "stringr",
+        "testthat",
+        "tibble",
+        "tidybayes", 
+        "tidyr", 
+        "usethis",
+        "wesanderson"
     )
 )
-options(tidyverse.quiet = TRUE)
+
+options(
+    mc.cores = 2,
+    brms.backend = "cmdstanr",
+    tidyverse.quiet = TRUE
+)
 
 # End this file with a list of target objects.
 list(
     
+    tar_target(
+        resolve_conficts,
+        {
+            # resolve namespace conflicts ----
+            conflict_prefer("last_warnings", "rlang")
+            conflict_prefer("filter", "dplyr")
+            conflict_prefer("between", "dplyr")
+            conflict_prefer("timestamp", "utils")
+            conflict_prefer("ar", "brms")
+            conflict_prefer("chisq.test", "stats")
+            conflict_prefer("discard", "scales")
+            conflict_prefer("duration", "lubridate")
+            conflict_prefer("fisher.test", "stats")
+            conflict_prefer("lag", "dplyr")
+        }
+    ),
+    
     # multilex data
-    tar_target(credentials, get_credentials()),
+    tar_target(
+        credentials,
+        get_credentials()
+    ),
+    
     tar_target(
         multilex_data,
         get_multilex(
-            update = FALSE,
+            update = TRUE, 
             type = c("understands", "produces")
         )
     ),
     
     # items
     tar_target(
-        cognate_codings_path,
-        here("Data", "cognate_codings.xlsx")
-    ),
-    tar_target(
-        cognate_codings,
-        read_xlsx(cognate_codings_path)
-    ),
-    tar_target(
-        items, 
+        items,
         get_items(
             multilex_data = multilex_data,
-            cognate_codings = cognate_codings,
-            impute = TRUE
         )
     ),
     
     # participants
     tar_target(
-        participants, 
+        participants,
         get_participants(
             multilex_data,
-            age_range = c(10, 36),
-            lps = c("Monolingual", "Bilingual"),
+            age = c(10, 36), 
+            lp = c("Monolingual", "Bilingual"), 
             other_threshold = 0.1
         )
     ),
@@ -64,7 +105,6 @@ list(
     tar_target(
         vocabulary,
         get_vocabulary(
-            participants = participants,
             multilex_data = multilex_data,
             type = c("understands", "produces")
         )
@@ -74,55 +114,232 @@ list(
     tar_target(
         responses,
         get_responses(
-            participants = participants,
-            items = items,
             multilex_data = multilex_data
         )
     ),
     
-    # fit models (comprehension)
     tar_target(
-        fits_comp,
-        fit_models_comp(
-            data = responses,
-            iter = 6000,
-            cores = 4,
-            chains = 4,
-            inits = 0,
-            save_pars = save_pars(all = TRUE),
-            backend = "cmdstanr"
+        df,
+        get_data(
+            participants = participants,
+            items = items,
+            responses = responses
         )
     ),
     
-    # fit models (production)
+    # mode prior
+    # these priors were set so that they generate data similar to what we expect
+    # they might not be perfectly sensible at the theoretical level
     tar_target(
-        fits_prod,
-        fit_models_prod(
-            data = responses,
-            iter = 6000,
-            cores = 4,
-            chains = 4,
-            inits = 0,
-            save_pars = save_pars(all = TRUE),
-            backend = "cmdstanr"
+        model_prior,
+        c(
+            prior(normal(-0.25, 0.1), class = "Intercept"),
+            prior(normal(1, 0.1), class = "sd", group = "te"),
+            prior(normal(1, 0.1), class = "sd", group = "id"),
+            prior(lkj(2), class = "cor"),
+            prior(normal(1, 0.1), class = "b", coef = "age_std"),
+            prior(normal(0, 0.1), class = "b", coef = "freq_std"),
+            prior(normal(0, 0.1), class = "b", coef = "n_phon_std"),
+            prior(normal(0, 0.1), class = "b", coef = "doe_std"),
+            prior(normal(0, 0.1), class = "b", coef = "lv_std"),
+            prior(normal(0, 0.1), class = "b", coef = "doe_std:lv_std"),
+            prior(normal(0, 0.1), class = "b", coef = "age_std:doe_std"),
+            prior(normal(0, 0.1), class = "b", coef = "age_std:lv_std"),
+            prior(normal(0, 0.1), class = "b", coef = "age_std:doe_std:lv_std")
         )
     ),
-
-    tar_target(
-        loo_comp,
-        compare_models(fits_comp)
-    ),
-
-    tar_target(
-        loo_prod,
-        compare_models(fits_prod)
-    ),
-
-    # render report.Rmd
-    tar_render(report, "Rmd/report.Rmd"),
     
-    # render manuscript
-    tar_render(manuscript, "Rmd/manuscript.Rmd")
+    # model formula
+    # multilevel model with crossed random effects (participants an items)
+    # responses are generated from a categorical distribution:
+    #   - https://journals.sagepub.com/doi/full/10.1177/2515245918823199
+    #   - https://cran.r-project.org/web/packages/brms/vignettes/brms_families.html
+    #   - https://bookdown.org/content/3686/ordinal-predicted-variable.html
+    # the probability of each response category is adjusted by age (population-level effect)
+    # and adjusted for each individual participant and item (group-level effects)
+    
+    # only intercepts (category boundaries)
+    tar_target(
+        model_fit_0,
+        fit_model(
+            name = "fit_0",
+            formula =  bf(
+                response ~ 1 +
+                    (1 | id) +
+                    (1 | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:3, ]
+        )
+    ),
+    
+    # add age main effect
+    tar_target(
+        model_fit_1,
+        fit_model(
+            name = "fit_1",
+            formula = bf(
+                response ~ age_std +
+                    (1 + age_std | id) +
+                    (1 + age_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:5, ]
+        )
+    ),
+    
+    # add age main effect
+    tar_target(
+        model_fit_2,
+        fit_model(
+            name = "fit_2",
+            formula = bf(
+                response ~ age_std + freq_std + 
+                    (1 + age_std + freq_std | id) +
+                    (1 + age_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:6, ]
+        )
+    ),
+    
+    # add dominance main effect
+    tar_target(
+        model_fit_3,
+        fit_model(
+            name = "fit_3",
+            formula = bf(
+                response ~ age_std + freq_std + n_phon_std +
+                    (1 + age_std + freq_std + n_phon_std | id) +
+                    (1 + age_std + freq_std + n_phon_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:7, ]
+        )
+    ),
+    tar_target(
+        model_fit_4,
+        fit_model(
+            name = "fit_4",
+            formula = bf(
+                response ~ age_std + freq_std + n_phon_std + doe_std +
+                    (1 + age_std + freq_std + n_phon_std + doe_std | id) +
+                    (1 + age_std + freq_std + n_phon_std + doe_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:8,]
+        )
+    ),
+    
+    # add age:dominance interaction
+    tar_target(
+        model_fit_5,
+        fit_model(
+            name = "fit_5",
+            formula = bf(
+                response ~ age_std + freq_std + n_phon_std + doe_std + lv_std + 
+                (1 + age_std + freq_std + n_phon_std + doe_std + lv_std | id) +
+                    (1 + age_std + freq_std + n_phon_std + doe_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:9,]
+        )
+    ),
+    # add age:dominance interaction
+    tar_target(
+        model_fit_6,
+        fit_model(
+            name = "fit_6",
+            formula = bf(
+                response ~ age_std + freq_std + n_phon_std + doe_std*lv_std + 
+                    (1 + age_std + freq_std + n_phon_std + doe_std*lv_std | id) +
+                    (1 + age_std + freq_std + n_phon_std + doe_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior[1:10,]
+        )
+    ),
+    tar_target(
+        model_fit_7,
+        fit_model(
+            name = "fit_7",
+            formula = bf(
+                response ~ age_std + freq_std + n_phon_std + doe_std*lv_std + age_std:(doe_std*lv_std) + 
+                    (1 + age_std + freq_std + n_phon_std + age_std:(doe_std*lv_std) | id) +
+                    (1 + age_std + freq_std + n_phon_std + age_std:doe_std | te),
+                family = cratio(link = "logit") # cumulative, continuation ratio
+            ), 
+            data = df,
+            prior = model_prior
+        )
+    ),
+    
+    # compare models ----
+    tar_target(
+        model_loos,
+        compare_models(
+            lst(
+                model_fit_0,
+                model_fit_1,
+                model_fit_2,
+                model_fit_3,
+                model_fit_4,
+                model_fit_5,
+                model_fit_6,
+                model_fit_7
+            ),
+            criterion = "loo",
+            cores = 4
+        )
+    ),
+    
+    # diagnose models ----
+    tar_target(
+        model_rhats,
+        map(
+            lst(
+                model_fit_0,
+                model_fit_1,
+                model_fit_2,
+                model_fit_3,
+                model_fit_4,
+                model_fit_5,
+                model_fit_6,
+                model_fit_7
+            ),
+            rhat
+        )
+    ),
+    tar_target(
+        model_neffs,
+        map(
+            lst(
+                model_fit_0,
+                model_fit_1,
+                model_fit_2,
+                model_fit_3,
+                model_fit_4,
+                model_fit_5,
+                model_fit_6,
+                model_fit_7
+            ),
+            neff_ratio
+        )
+    )
+    
+    
+    # # render report.Rmd
+    # tar_render(report, "docs/report.Rmd"),
+    # 
+    # # render manuscript
+    # tar_render(manuscript, "manuscript/manuscript.Rmd")
     
 )
 
