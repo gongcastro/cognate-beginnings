2199be72fe7a1ef6a2a586accdb4ca6ebf781b2b
Gonzalo García-Castro <31894043+gongcastro@users.noreply.github.com>
2022-08-02 11:26:51 +0200

    Add IRT models

---
+++ R/02_models.R
@@ -0,0 +1,51 @@
+
+# specify and sample model ----
+
+fit_model <- function(name, ...){
+    # we run the model in the background as an RStudio job to keep the console free
+    fit <- brm(
+        ...,
+        sample_prior = "yes", # for faster computation of Bayes Factors and LOO
+        iter = 4000,
+        chains = 2,
+        init = 0, # where to initialise MCMCs
+        seed = 888, # for reproducibility
+        backend = "cmdstanr", # for faster, less problematic compilation in C++
+        file = here("results", paste0(name, ".rds")), # save model as file
+        # file_refit = "always", # should model be refitted or loaded from file?
+        control = list(
+            adapt_delta = 0.9, # for better convergence of MCMCs
+            max_treedepth = 15
+        ),
+        save_model = here("stan", paste0(name, ".stan")) # save Stan code
+    )
+    
+    return(fit)
+}
+
+
+# compare models ----
+compare_models <- function(x, criterion = "loo", ...) {
+    
+    if (criterion=="loo") {
+        
+        # models are too larger, compute LOO from subsamples (500 by default)
+        loos <- lapply(x, loo_subsample, ...)
+        
+        saveRDS(
+            loos,
+            here("results", paste0("model_", criterion, ".rds"))
+        )
+        
+    } else {
+        
+        # future versions of this function might allow other criteria like WAIC
+        stop("Criterion must be one of: 'loo'")
+        
+    }
+    return(loos)
+}
+
+
+
+
