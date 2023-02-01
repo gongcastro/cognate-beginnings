4941e81dd003b9ba428c10dabf8b2eb7d91e3e2c
Gonzalo García-Castro <31894043+gongcastro@users.noreply.github.com>
2023-02-01 17:00:49 +0100

    Update tests

---
+++ tests/testthat/test-participants.R
@@ -0,0 +1,81 @@
+test_participants <- function(participants) {
+    test_that("participants variable names are right", {
+        expect_true(all(
+            colnames(participants) %in% c(
+                "id",
+                "id_bvq",
+                "time",
+                "time_stamp",
+                "list",
+                "age",
+                "lp",
+                "doe_catalan",
+                "doe_spanish",
+                "edu_parent"
+            )
+        ))
+    })
+    
+    test_that("participants variable classes are right", {
+        expect_type(participants$id, "integer")
+        expect_type(participants$time, "integer")
+        expect_type(participants$time_stamp, "double")
+        expect_type(participants$list, "character")
+        expect_type(participants$age, "double")
+        expect_type(participants$lp, "character")
+        expect_type(participants$doe_catalan, "double")
+        expect_type(participants$doe_spanish, "double")
+        expect_type(participants$edu_parent, "integer")
+    })
+    
+    test_that("same id does not repeat time", {
+        expect_equal(max(count(participants, id, time)$n), 1)
+    })
+    
+    test_that("list has appropriate values", {
+        expect_true(all(
+            participants$list %in% c(
+                "BL-Long-1",
+                "BL-Long-2",
+                "BL-Short-A",
+                "BL-Short-B",
+                "BL-Short-C",
+                "BL-Short-D",
+                "BL-Lockdown-A",
+                "BL-Lockdown-B",
+                "BL-Lockdown-C",
+                "BL-Lockdown-D"
+            )
+        ))
+    })
+    
+    test_that("age is positive and within the appropriate range", {
+        expect_gt(min(participants$age), 12)
+        expect_lte(max(participants$age), 34)
+    })
+    
+    test_that("lp is 'Monolingual' or 'Bilingual'", {
+        expect_true(all(participants$lp %in% c("Monolingual", "Bilingual")))
+    })
+    
+    test_that("doe_catalan is between 0 and 1", {
+        expect_gte(min(participants$doe_catalan), 0)
+        expect_lte(max(participants$doe_catalan), 1)
+    })
+    
+    test_that("doe_spanish is between 0 and 1", {
+        expect_gte(min(participants$doe_spanish), 0)
+        expect_lte(max(participants$doe_spanish), 1)
+    })
+    
+    test_that("sum of doe_spanish and doe_catalan does not exceed 1", {
+        expect_lte(max(participants$doe_catalan + participants$doe_spanish),
+                   1)
+    })
+    
+    test_that("sum of doe_spanish and doe_catalan exceeds or is equal to 0.8",
+              {
+                  expect_gte(min(participants$doe_catalan + participants$doe_spanish),
+                             0.8)
+              })
+}
--- tests/testthat/test-responses.R
+++ tests/testthat/test-responses.R
@@ -1,53 +1,60 @@
-tar_load(responses)
-
-test_that("response variable names are right", {
-    expect_true(
-        all(colnames(responses) %in% c("id", "time", "code", "language", "item", "response"))
-    )
-})
-
-test_that("response variable classes are right", {
-    expect_type(responses$id, "character")
-    expect_type(responses$time, "integer")
-    expect_type(responses$code, "character")
-    expect_type(responses$item, "character")
-    expect_type(responses$response, "integer")
-})
-
-test_that("all codes have more than 100 associated responses", {
-    expect_true(
-        responses %>% 
-            count(code) %>% 
-            pull(n) %>% 
-            all(. > 100)
-    )
-})
-
-test_that("all codes have less than 800 associated responses", {
-    expect_true(
-        responses %>% 
-            count(code) %>% 
-            pull(n) %>% 
-            all(. < 800)
-    )
-})
-
-test_that("no times are duplicated for the same ID", {
-    expect_true(
-        responses %>% 
-            count(id, time, item) %>% 
-            pull(n) %>% 
-            all(. < 2)
-    )
-})
-
-test_that("no codes are duplicated", {
-    expect_true(
-        responses %>% 
-            distinct(id, time, code) %>% 
-            count(code) %>% 
-            pull(n) %>% 
-            all(. < 2)
-    )
-})
-
+test_responses <- function(responses) {
+    test_that("responses variable names are right", {
+        expect_true(all(
+            colnames(responses) %in% c(
+                "id",
+                "time",
+                "age",
+                "age_std",
+                "te",
+                "language",
+                "meaning",
+                "item",
+                "response",
+                "lv",
+                "lv_std",
+                "freq",
+                "freq_std",
+                "n_phon",
+                "n_phon_std",
+                "doe",
+                "doe_std",
+                "exposure",
+                "exposure_std"
+            )
+        ))
+    })
+    
+    test_that("responses variable classes are right", {
+        expect_type(responses$id, "integer")
+        expect_type(responses$time, "integer")
+        expect_type(responses$age, "double")
+        expect_type(responses$age_std, "double")
+        expect_type(responses$te, "integer")
+        expect_type(responses$language, "character")
+        expect_type(responses$meaning, "character")
+        expect_type(responses$item, "character")
+        expect_type(responses$response, "integer")
+        expect_type(responses$lv, "double")
+        expect_type(responses$lv_std, "double")
+        expect_type(responses$freq, "double")
+        expect_type(responses$freq_std, "double")
+        expect_type(responses$n_phon, "integer")
+        expect_type(responses$n_phon_std, "double")
+        expect_type(responses$doe, "double")
+        expect_type(responses$doe_std, "double")
+        expect_type(responses$exposure, "double")
+        expect_type(responses$exposure_std, "double")
+        
+    })
+    
+    test_that("response take values 1, 2, or 3", {
+        expect_true(all(unique(responses$response) %in% c("No",
+                                                          "Understands",
+                                                          "Understands and Says")))
+    })
+    
+    test_that("no times are duplicated for the same ID", {
+        expect_lte(max(count(responses, id, time, language, item)$n), 1)
+    })
+}
